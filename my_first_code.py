dc9928670fe5954d2e2a012a972e08a9aa64c18b
ERaileanu <elvira.raileanu79@gmail.com>
2024-09-17 21:55:51 +0100

    Add files via upload

---
+++ my_first_code.py
@@ -0,0 +1,50 @@
+```python
+# This is my first line of code
+print("My name is Elvira")
+print("The date is 17/09/2024")
+```
+
+    My name is Elvira
+    The date is 17/09/2024
+    
+
+
+```python
+# Write a program to analyse real estate data. Print the total sales from December
+A = 45000
+B = 23400
+C = 67000
+D = 34600
+E = 12900
+total_sales = A+B+C+D+E
+print(total_sales)
+```
+
+    182900
+    
+
+
+```python
+#Print the average sales price for the month of December
+average_sales = total_sales/5
+print(average_sales)
+```
+
+    36580.0
+    
+
+
+```python
+#Print the property price for Property C
+C = 67000
+C -= 6500
+print(C)
+```
+
+    60500
+    
+
+
+```python
+
+```
